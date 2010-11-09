3c44d838e57ffb7a00273c73755a763457092c51
Elijah C Menifee <ElijahCMenifee@gmail.com>
2010-11-08 19:17:22 -0600

    Changed Point3D to follow Java equals/hashCode. Changed hashCode and comments to be inline with a java object meaning. Changed equals   and comments to be inline with standard java object meaning. Changed trulyEquals to almostEquals as old meaning of equals. Added almostEquals method that takes a modified ULPS value. ***NOTE*** none of the methods are yet implemented, finishing UnitTests first

--- src/org/tonoplace/glmotes/geometry/Point3D.java
+++ src/org/tonoplace/glmotes/geometry/Point3D.java
@@ -76,6 +76,12 @@ public class Point3D implements Serializable
   private double z_;
 
   /**
+   * Caches the hashCode value, once needed to speed up subsequent calls, since
+   * it can be more expensive to perform the additional math for the hashCode
+   */
+  private Integer hashCodeCache_ = null;
+
+  /**
    * Constructs and initializes a Point3D with coordinates(0,0,0).
    */
   Point3D()
@@ -161,24 +167,107 @@ public class Point3D implements Serializable
     return "";
   }
 
+  //
+  // Returns a hash code for this Point3D.  Because the equals method fudges the
+  // comparison so that extremely close Point3Ds are considered equal, a hash
+  // algorithm that would always produce the same hashCode for any two Points
+  // that are considered equal has not been devised, therefore the hashCode for
+  // all values are 0 to ensure the hashCode is compliant with the equals method
+  // @return a hashCode of 0.
+  //
   /**
-   * Returns a hash code for this Point3D.  Because the equals method fudges the
-   * comparison so that extremely close Point3Ds are considered equal, a hash
-   * algorithm that would always produce the same hashCode for any two Points
-   * that are considered equal has not been devised, therefore the hashCode for
-   * all values are 0 to ensure the hashCode is compliant with the equals method
-   * @return a hashCode of 0.
+   * Returns a hash code value for the object. This method is supported for the
+   * benefit of hashtables such as those provided by java.util.Hashtable.
+   * <br>
+   * The general contract of hashCode is:
+   * <br>
+   * <ul>
+   *   <li>•Whenever it is invoked on the same object more than once during an
+   *        execution of a Java application, the hashCode method must
+   *        consistently return the same integer, provided no information used
+   *        in equals comparisons on the object is modified. This integer need
+   *        not remain consistent from one execution of an application to
+   *        another execution of the same application.<li>
+   *   <li>•If two objects are equal according to the equals(Object) method,
+   *        then calling the hashCode method on each of the two objects must
+   *        produce the same integer result.</li>
+   *   <li>•It is not required that if two objects are unequal according to the
+   *        equals(java.lang.Object) method, then calling the hashCode method
+   *        on each of the two objects must produce distinct integer results.
+   *        However, the programmer should be aware that producing distinct
+   *        integer results for unequal objects may improve the performance of
+   *        hashtables.</li>
+   * </ul>
+   *
+   * @return a hash code value for this object
+   *
+   * @see equals(Object obj)
    */
   @Override
   public int hashCode()
   {
-    // becuase we allow an almost equal double calculation for equals, only safe
-    // way to make sure the hash codes are equal for objects that are equal is
-    // to always return the same hashCode, Point3Ds will not be efficiant as
-    // keys in a int hash based algorithm.
     return 0;
   }
 
+  
+//   Compares this object against the specified object.  This method will only
+//   report true if obj is not null and is a Point3d.  If obj is a valid Point3D
+//   than each of its coordinates are compared using a nearly equal algorithm so
+//   that two points that are extremely close are considered equal.  This is to
+//   take into account the problem of precision of floating point calculations
+//   where a change in the order of operations can affect the comparison of
+//   float values. Due to the nature of this nearly equal comparison given 3
+//   points A, B, and C: The method may report A==B and B==C but A!=C.
+//
+//   @see FloatingPointComparison.AlmostEqual2sComplement(double a, double b, long maxULPS)
+//   @param obj the object to compare with.
+//   @return true if the points represent the same point, within the accepted
+//   amount of floating point error.
+//
+  /**
+   * Indicates whether some other object is "equal to" this one.
+   * The equals method implements an equivalence relation on non-null object references:
+   *
+   * <ul>
+   *  <li>•It is reflexive: for any non-null reference value x, x.equals(x)
+   * should return true. </li>
+   *  <li>•It is symmetric: for any non-null reference values x and y,
+   * x.equals(y) should return true if and only if y.equals(x) returns true.
+   * </li>
+   *  <li>•It is transitive: for any non-null reference values x, y, and z,
+   * if x.equals(y) returns true and y.equals(z) returns true, then x.equals(z)
+   * should return true. </li>
+   *  <li>•It is consistent: for any non-null reference values x and y,
+   * multiple invocations of x.equals(y) consistently return true or
+   * consistently return false, provided no information used in equals
+   * comparisons on the objects is modified. </li>
+   *  <li>•For any non-null reference value x, x.equals(null) should return
+   * false. </li>
+   *</ul>
+   * Note that it is generally necessary to override the hashCode method 
+   * whenever this method is overridden, so as to maintain the general contract
+   * for the hashCode method, which states that equal objects must have equal
+   * hash codes.
+   * @param obj the reference object with which to compare.
+   * @return true if this object has the same values as the obj argument;
+   * false otherwise.
+   * @see hashCode()
+   */
+  @Override
+  public boolean equals(Object obj)
+  {
+    return false;
+  }
+
+//
+//   Compares this object against the specified object for exact value.  Using
+//   this to compare can lead to points not being reported as equal that would
+//   be considered equal if not for floating point epsilon errors.  This method
+//   is the equivalent call of equals that most objects use to define equals.
+//   @param otherPoint the object to compare with.
+//   @return true if otherPoint is not null and the X,Y, and Z coordinates are
+//   exactly the same.
+//
   /**
    * Compares this object against the specified object.  This method will only
    * report true if obj is not null and is a Point3d.  If obj is a valid Point3D
@@ -194,9 +283,9 @@ public class Point3D implements Serializable
    * @return true if the points represent the same point, within the accepted
    * amount of floating point error.
    */
-  @Override
-  public boolean equals(Object obj)
+  public boolean almostEquals(Point3D otherPoint)
   {
+    return false;
     /**
     if (obj==null)
       return false;
@@ -222,25 +311,29 @@ public class Point3D implements Serializable
     }
     return false;
      **/
+  }
+  /**
+   * Compares this object against the specified object.  This method will only
+   * report true if obj is not null and is a Point3d.  If obj is a valid Point3D
+   * than each of its coordinates are compared using a nearly equal algorithm so
+   * that two points that are extremely close are considered equal.  This is to
+   * take into account the problem of precision of floating point calculations
+   * where a change in the order of operations can affect the comparison of
+   * float values. Due to the nature of this nearly equal comparison given 3
+   * points A, B, and C: The method may report A==B and B==C but A!=C.
+   *
+   * @param obj the object to compare with.
+   * @param ulps the ULPS value to use for considering if obj is almost equal.
+   * @return true if the points represent the same point, within the accepted
+   * amount of floating point error.
+   * @see FloatingPointComparison.AlmostEqual2sComplement(double a, double b, long maxULPS)
+   */
+  public boolean almostEquals(Point3D otherPoint, long ULPS)
+  {
     return false;
   }
 
-  /**
-   * Compares this object against the specified object for exact value.  Using
-   * this to compare can lead to points not being reported as equal that would
-   * be considered equal if not for floating point epsilon errors.  This method
-   * is the equivalent call of equals that most objects use to define equals.
-   * @param otherPoint the object to compare with.
-   * @return true if otherPoint is not null and the X,Y, and Z coordinates are
-   * exactly the same.
-   */
-  public boolean trulyEquals(Point3D otherPoint)
-  {
-//    if (otherPoint==null)
-//      return false;
-//    return x_==otherPoint.x_ && y_==otherPoint.y_ && z_==otherPoint.z_;
-    return false;
-  }
+
 
   /**
    * Calculates the distance between this Point3D and the specified Point3D.
